746bc070e01c6f6b1f9a597332c79fb50fbafa6c
nisam <nmtkdr10@gmail.com>
2023-09-26 20:06:26 +0530

    2

---
+++ lib/models/Project.dart
@@ -0,0 +1,34 @@
+class Project {
+  final String? title, description;
+
+  Project({this.title, this.description});
+}
+
+List<Project> demo_projects = [
+  Project(
+    title: "iCart E-Commerce App",
+    description:
+    "iCart, the ultimate e-commerce destination for all your mobile accessory needs. With iCart, you can effortlessly browse, shop, and complete transactions, all while managing your virtual wallet. Explore a vast selection of top-quality mobile accessories and enjoy a range of convenient payment options.",
+  ),
+  Project(
+    title: "FursanCart E-Commerce App",
+    description:
+        "FursanCart is your trusted e-commerce destination for all things electronics. Seamlessly integrated with Razorpay, our vendor app empowers electronics sellers to reach a vast customer base. Join us in revolutionizing the electronics marketplace and deliver quality products to tech enthusiasts worldwide..",
+  ),
+  Project(
+    title: "Wasellee Drivers and Traders Logistics App",
+    description:
+        "Wasellee is your go-to courier service app, exclusively tailored for Qatar. Whether you're a driver or trader, Wasellee connects you to a network of logistics solutions designed to thrive in the vibrant Qatari market. Experience seamless, on-demand courier services that keep Qatar moving..",
+  ),
+  Project(
+    title: "Vydha Online Doctor Consultation App",
+    description:
+        "Vydha is your trusted online doctor consultation app, bridging the gap between you and healthcare. Connect seamlessly with video and audio calls to experienced doctors, all while making secure payments through Razorpay. Your health, your convenience – Vydha has you covered..",
+  ),
+  Project(
+    title: "Elcon Home Services and Complete Solutions",
+    description:
+        "Elcon is your ultimate home services app, offering a comprehensive solution for all your household needs. From repairs and maintenance to expert consultations, Elcon connects you to trusted professionals, ensuring convenience and peace of mind in every task. Simplify your life with Elcon..",
+  ),
+
+];
---
+++ lib/screens/main/components/coding.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_profile/components/animated_progress_indicator.dart';
+
+import '../../../constants.dart';
+
+class Coding extends StatelessWidget {
+  const Coding({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+          child: Text(
+            "Coding",
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.9,
+          label: "Dart",
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.25,
+          label: "HTML",
+        ),
+
+        AnimatedLinearProgressIndicator(
+          percentage: 0.25,
+          label: "JavaScript",
+        ),
+      ],
+    );
+  }
+}
